3735293d2c13eec84d82366468b8dfbe3fb85047
Jack Lee <weeyilee@hotmail.com>
2018-10-29 17:57:58 +0800

    First commit

--- run_analysis.R
+++ run_analysis.R
@@ -14,16 +14,15 @@
 ## 4. Appropriately labels the data set with descriptive variable names.
 ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-Cleaningdataproject <- function () {
 
-	## install the needed packages if not loaded yet
+## install the needed packages if not loaded yet
 	if(!library(reshape2, logical.return = TRUE)) {
  	 install.packages('reshape2')
  	 library(reshape2)
 	}
 
 
-	## download the data files
+## download the data files
 	if(!file.exists("./data")) {dir.create("./data")}
 	fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 	download.file(fileUrl,destfile="./data/data.zip")
@@ -88,7 +87,3 @@ Cleaningdataproject <- function () {
 	MeltedData <- melt(CleanedData, id = c("Subject","Activities"))
 	tidyData <- dcast(MeltedData, Subject + Activities ~ variable, mean)
 	write.table(tidyData,file=file.path("./data/tidy.txt"),row.name=FALSE)
-
-
-	
-}
